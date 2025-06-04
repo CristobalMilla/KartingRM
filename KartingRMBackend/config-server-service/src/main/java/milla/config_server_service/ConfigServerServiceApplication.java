1fd45d78c16a12b50d34d095adb3935339efd184
Cristobal Milla <cristobal.milla@usach.cl>
2025-06-03 20:10:27 -0400

    Config, eureka y gateweay funcionando.

--- KartingRMBackend/config-server-service/src/main/java/milla/config_server_service/ConfigServerServiceApplication.java
+++ KartingRMBackend/config-server-service/src/main/java/milla/config_server_service/ConfigServerServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServiceApplication {
+public class ConfigServerServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServiceApplication.class, args);
+		SpringApplication.run(ConfigServerServiceApplication.class, args);
 	}
 
 }
