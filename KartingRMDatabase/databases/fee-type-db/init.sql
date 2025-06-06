1c6af3d83943bb2ca2f0ab69a907480fb24ee1e5
Cristobal Milla <cristobal.milla@usach.cl>
2025-06-06 09:49:41 -0400

    Archivos docker db, cambios en microservices bootstrap
    Microservicios sin bases de datos deberian correr.
    Dcokers de cada servicio y bases de datos deberian correr.

--- KartingRMDatabase/databases/fee-type-db/init.sql
+++ KartingRMDatabase/databases/fee-type-db/init.sql
@@ -5,3 +5,8 @@ CREATE TABLE fee_type (
     price NUMERIC(10, 2) NOT NULL,
     duration INT NOT NULL
 );
+INSERT INTO fee_type (lap_number, max_time, price, duration)
+VALUES 
+    (10, 15000, 10.00, 30),
+    (15, 20000, 15.00, 35),
+    (20, 25000, 20.00, 40);
